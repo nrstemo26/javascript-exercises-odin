5a95b05066e19c59d56eb1a18f94328820bd34de
Nate Stemo <natestemo@hotmail.com>
2022-07-15 12:32:27 -0500

    solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let newStr = '';
+    for(let i = 0; i < num; i++){
+        newStr += str;
 
+    }
+    console.log(newStr)
+    return newStr;
 };
 
 // Do not edit below this line
